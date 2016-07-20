d60bac894463c4193c4516664c9ccad1ef3bca13
Marco Scoppetta <marco@mindmaps.io>
2016-07-20 10:56:12 +0100

    working on web dashboard

--- mindmaps-engine/src/main/java/io/mindmaps/api/RestGETController.java
+++ mindmaps-engine/src/main/java/io/mindmaps/api/RestGETController.java
@@ -3,6 +3,8 @@ package io.mindmaps.api;
 import io.mindmaps.core.implementation.MindmapsTransactionImpl;
 import io.mindmaps.factory.GraphFactory;
 import io.mindmaps.graql.api.parser.QueryParser;
+import spark.Request;
+import spark.Response;
 
 import java.util.stream.Collectors;
 
@@ -13,19 +15,30 @@ public class RestGETController {
     MindmapsTransactionImpl graphTransaction;
 
     public RestGETController() {
-
         graphTransaction = GraphFactory.getInstance().buildMindmapsGraph();
 
         get("/hello", (req, res) -> "こんにちは from Mindmaps Engine!");
 
-        get("/select", (req, res) -> {
-            QueryParser parser = QueryParser.create(graphTransaction);
-            System.out.println("RECEIVED SELECT QUERY "+req.queryParams("query"));
-
-            return parser.parseMatchQuery(req.queryParams("query")).resultsString()
-                    .map(x -> x.replaceAll("\u001B\\[\\d+[m]", ""))    // instead of replacing use the code in the web page to highlight the syntax
-                    .collect(Collectors.joining("\n"));
-        });
-
+        get("/match", this::matchQuery);
     }
+
+
+    private String matchQuery(Request req, Response res) {
+        QueryParser parser = QueryParser.create(graphTransaction);
+        System.out.println("RECEIVED SELECT QUERY " + req.queryParams("query"));
+
+        try {
+            return parser.parseMatchQuery(req.queryParams("query"))
+                    .resultsString()
+                    .map(x -> x.replaceAll("\u001B\\[\\d+[m]", ""))    // instead of replacing use the code in the web page to highlight the syntax (?)
+                    .collect(Collectors.joining("\n"));
+        }catch(Exception e){
+            res.status(400);
+            return e.getMessage();
+        }
+    }
+
+
+
+
 }
--- mindmaps-engine/src/main/resources/public/js/graql_shell.js
+++ mindmaps-engine/src/main/resources/public/js/graql_shell.js
@@ -1,24 +1,36 @@
 
-function get(url, callback) {
-
-        $.get(url, function (data) {
-            callback(data);
-        })
-
-}
+//function get(url, callback) {
+//
+//        $.get(url, function (data) {
+//            callback(data);
+//        })
+//
+//}
 
 $(function() {
     $("#graql-result").hide();
+    $("#query-error").hide();
+    $("#toggler").hide();
 
     $( "#sendquery" ).click(function(){
         $('#myPleaseWait').modal('show');
-        get("http://"+$(location).attr('host')+"/select?query=" + $("#graql-string").val(), function (data) {
+        $.get("http://"+$(location).attr('host')+"/match?query=" + $("#graql-string").val(), function (data) {
             $('#myPleaseWait').modal('hide');
+            $("#toggler").show();
+            $("#query-error").hide();
             $("#graql-result").val(data);
             $("#graql-result").show();
             $("#graql-result").resize();
+        }).fail(function(data){
+            $('#myPleaseWait').modal('hide');
+            $("#toggler").hide();
+            $("#graql-result").hide();
+            $("#query-error").html("<span><strong> Oh snap! </strong></span> Error: "+JSON.stringify(data.responseText)+"<span id='close-error' ><strong> X </strong></span>");
+            $("#query-error").slideDown();
         });
     });
+
+    $(document).on("click","#close-error",function(){    $("#query-error").slideUp();});
 
     $( "#toggler" ).click(function() {
         $( ".query-row" ).slideToggle( "slow", function() {
